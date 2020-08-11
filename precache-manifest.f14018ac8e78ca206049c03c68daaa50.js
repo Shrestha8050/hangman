9d218147bc64d82371e010efc50e8c337b3785e1
Bodhan Shrestha <bodhanshrestha@gmail.com>
2020-08-11 07:57:52 +0545

    Updates

---
+++ precache-manifest.f14018ac8e78ca206049c03c68daaa50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90d3d04b80260b5739165d5067a319ea",
+    "url": "/hangman/index.html"
+  },
+  {
+    "revision": "3d5e7e2d2a6eb5abf417",
+    "url": "/hangman/static/css/main.7a5c9aef.chunk.css"
+  },
+  {
+    "revision": "36790b53a96eec4bcc02",
+    "url": "/hangman/static/js/2.ad1728cd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hangman/static/js/2.ad1728cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d5e7e2d2a6eb5abf417",
+    "url": "/hangman/static/js/main.cd7f2167.chunk.js"
+  },
+  {
+    "revision": "7ef95293683d0ed87720",
+    "url": "/hangman/static/js/runtime-main.6b115b9b.js"
+  }
+]);
